8c180cd1595267d170b5fa20f0d024d7f759a78a
lauraliu518 <yixuanliu0518@gmail.com>
2026-05-20 14:50:16 -0400

    base template

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    # Pass dynamic variables directly to the template
-    return render_template('basic_info.html', title="Home Page", user="Developer")
+@app.route('/', methods=['GET', 'POST'])
+def basic_info():
+    raters = ['Alice', 'Bob', 'Cindy', 'David']
+    return render_template('basic_info.html', raters=raters)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
