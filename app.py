84c4debdd3e75aa178c8aa710b30270a42d37f4e
lauraliu518 <yixuanliu0518@gmail.com>
2026-05-20 20:18:40 -0400

    all els, css

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+# dummy data for now
+raters = ['', 'Alice', 'Bob', 'Cindy', 'David']
+sites = ['', 'Bliss', 'Drum', 'JBLM']
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def basic_info():
-    raters = ['Alice', 'Bob', 'Cindy', 'David']
-    return render_template('basic_info.html', raters=raters)
+    return render_template('basic_info.html', raters=raters, sites=sites)
+
+@app.route('/maltreatment-category')
+def maltreatment_category():
+    return render_template('maltreatement_category.html')
 
 
 
